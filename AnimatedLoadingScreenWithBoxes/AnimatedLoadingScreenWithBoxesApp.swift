3a72c330ee855c369046b346f2363421673d4aa6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-03-18 14:45:55 +0200

    Initial Commit

---
+++ AnimatedLoadingScreenWithBoxes/AnimatedLoadingScreenWithBoxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedLoadingScreenWithBoxesApp.swift
+//  AnimatedLoadingScreenWithBoxes
+//
+//  Created by ramil on 18.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedLoadingScreenWithBoxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedLoadingScreenWithBoxes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedLoadingScreenWithBoxes
+//
+//  Created by ramil on 18.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
